44420e844eb682acd1cf4a0b07416d38617d4a9c
MartinGrue <gruebler.martin@outlook.de>
2020-08-17 21:23:08 +0200

    .

--- API/src/index.ts
+++ API/src/index.ts
@@ -6,22 +6,25 @@ import { User } from "./models/User";
 import { seedData } from "./seed";
 import { signInRouter, currentUserRouter } from "./routes/auth/";
 import { usersRouter } from "./routes/users";
+import cors from "cors";
 import cookieSession from "cookie-session";
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(cors());
+
+const port = process.env.PORT || 4000;
 app.use(json());
 app.use(cookieSession({ signed: false, secure: false }));
 app.use(signInRouter);
 app.use(currentUserRouter);
 app.use(usersRouter);
-const mongoConnectionString = "'mongodb://localhost:27017'";
+
 // mongoose.connect(mongoConnectionString, {
 //   useNewUrlParser: true,
 //   useCreateIndex: true,
 // });
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/limitless-depths-40520"
+  process.env.MONGODB_URI || "mongodb://localhost:27017"
 );
 mongoose.connection.on("connected", () => {
   console.log("Connected to mongo instance");
@@ -31,9 +34,10 @@ mongoose.connection.on("error", (error: any) => {
 });
 
 // console.log(users);
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/auth", (req, res) => {
+//   console.log("Hello", req.url)
+//   res.send("Hello World!");
+// });
 app.listen(port, async () => {
   console.log("Im listening on port:", port);
   await User.find({}, (err, users) => {
